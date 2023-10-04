d5a1cf51b881aed50003f8fcb4efc06d40444a5f
Pierce Cirks <piercec@bulwarkpest.com>
2023-10-04 12:28:01 -0700

    Pierce

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="I am Pierce" />
+      <HelloWorld msg="I am awesome" />
     </div>
   </header>
 
